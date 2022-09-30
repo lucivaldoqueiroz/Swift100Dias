98029bb23b1dd97f450dc3c96db42f8c40f56240
Lucivaldo Quiroz <lucivaldo.queiroz@gmail.com>
2022-09-29 23:12:13 -0300

    Dia 5/100 – if, switch, e operadores ternarios

---
+++ OperadorTernario.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+let age = 18
+                    //what, true, false
+let canVote = age >= 18 ? "Yes" : "No"
+
+let hour = 23
+print(hour < 12 ? "It's before noon" : "It's after noon")
+
+let names = ["Jayne", "Kaylee", "Mal"]
+let crewCount = names.isEmpty ? "No one" : "\(names.count) people"
+print(crewCount)
+
+enum Theme {
+    case light, dark
+}
+
+let theme = Theme.dark
+
+let background = theme == .dark ? "black" : "white"
+print(background)
